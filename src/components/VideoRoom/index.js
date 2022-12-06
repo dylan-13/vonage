750cc8e006fe16c9637a58a30a020e3779388cbd
dylan-13 <dylan.berthier13@gmail.com>
2022-12-06 16:08:34 +0100

    fix

--- src/components/VideoRoom/index.js
+++ src/components/VideoRoom/index.js
@@ -1,24 +1,32 @@
-import { useParams } from "react-router";
-import { useContext, useEffect, useRef, useState } from "react";
-import { getCredentials } from "../../api/fetchCreds";
-import styles from "./styles.js";
-import useRoom from "../../hooks/useRoom";
-import { UserContext } from "../../context/UserContext";
+import NetworkToast from 'components/NetworkToast'
+import ToolBar from 'components/ToolBar'
+import { useContext, useEffect, useRef, useState } from 'react'
+import { useParams } from 'react-router'
 
-import ToolBar from "components/ToolBar";
-
-import NetworkToast from "components/NetworkToast";
-import useScreenSharing from "../../hooks/useScreenSharing";
+import { getCredentials } from '../../api/fetchCreds'
+import { UserContext } from '../../context/UserContext'
+import useRoom from '../../hooks/useRoom'
+import useScreenSharing from '../../hooks/useScreenSharing'
+import styles from './styles.js'
 
 export default function VideoRoom() {
-  const { user } = useContext(UserContext);
-  const [credentials, setCredentials] = useState(null);
-  const [error, setError] = useState(null);
-  const { createCall, room, participants, connected, networkStatus, cameraPublishing, localParticipant } = useRoom();
-  const { isScreenSharing, startScreenSharing, stopScreenSharing } = useScreenSharing({ room });
-  const roomContainer = useRef();
-  const classes = styles();
-  let { roomName } = useParams();
+  const { user } = useContext(UserContext)
+  const [credentials, setCredentials] = useState(null)
+  const [error, setError] = useState(null)
+  const {
+    createCall,
+    room,
+    participants,
+    connected,
+    networkStatus,
+    cameraPublishing,
+    localParticipant,
+  } = useRoom()
+  const { isScreenSharing, startScreenSharing, stopScreenSharing } =
+    useScreenSharing({ room })
+  const roomContainer = useRef()
+  const classes = styles()
+  let { roomName } = useParams()
 
   useEffect(() => {
     getCredentials(roomName)
@@ -27,30 +35,52 @@ export default function VideoRoom() {
           apikey: data.apiKey,
           sessionId: data.sessionId,
           token: data.token,
-        });
+        })
       })
       .catch((err) => {
-        setError(err);
-        console.log(err);
-      });
-  }, [roomName]);
+        setError(err)
+        console.log(err)
+      })
+  }, [roomName])
 
   useEffect(() => {
     if (credentials) {
-      createCall(credentials, roomContainer.current, user.userName, user.videoFilter, {
-        ...user.defaultSettings,
-      });
+      createCall(
+        credentials,
+        roomContainer.current,
+        user.userName,
+        user.videoFilter,
+        {
+          ...user.defaultSettings,
+        }
+      )
     }
-  }, [createCall, credentials, user]);
+  }, [createCall, credentials, user])
 
-  if (error) return <div className={classes.errorContainer}>There was an error fetching the data from the server</div>;
+  if (error)
+    return (
+      <div className={classes.errorContainer}>
+        There was an error fetching the data from the server
+      </div>
+    )
 
   return (
     <div id="callContainer" className={classes.callContainer}>
-      <div id="roomContainer" className={classes.roomContainer} ref={roomContainer}>
+      <div
+        id="roomContainer"
+        className={classes.roomContainer}
+        ref={roomContainer}
+      >
         <NetworkToast networkStatus={networkStatus} />
-        <div id="screenSharingContainer" className={classes.screenSharingContainer}>
-          {isScreenSharing && <div className={classes.screenSharingOverlay}>You Are Screensharing</div>}
+        <div
+          id="screenSharingContainer"
+          className={classes.screenSharingContainer}
+        >
+          {isScreenSharing && (
+            <div className={classes.screenSharingOverlay}>
+              You Are Screensharing
+            </div>
+          )}
         </div>
       </div>
       <ToolBar
@@ -64,5 +94,5 @@ export default function VideoRoom() {
         stopScreenSharing={stopScreenSharing}
       ></ToolBar>
     </div>
-  );
+  )
 }
