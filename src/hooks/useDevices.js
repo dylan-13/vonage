82420d1c6513c9fc111213e2995441d9964f617c
dylan-13 <dylan.berthier13@gmail.com>
2022-12-07 16:23:03 +0100

    build

--- src/hooks/useDevices.js
+++ src/hooks/useDevices.js
@@ -1,50 +1,50 @@
-import { useState, useEffect, useCallback } from 'react';
-import * as VideoExpress from '@vonage/video-express';
+import * as VideoExpress from '@vonage/video-express'
+import { useCallback, useEffect, useState } from 'react'
 
 export default function useDevices() {
   const [deviceInfo, setDeviceInfo] = useState({
     audioInputDevices: [],
     videoInputDevices: [],
     audioOutputDevices: [],
-  });
+  })
 
   const getDevices = useCallback(async () => {
     if (!navigator.mediaDevices || !navigator.mediaDevices.enumerateDevices) {
-      console.warn('enumerateDevices() not supported.');
-      return;
+      console.warn("enumerateDevices() not supported.")
+      return
     }
     try {
-      const devices = await VideoExpress.getDevices();
-      let audioOutputDevices = await VideoExpress.getAudioOutputDevices();
+      const devices = await VideoExpress.getDevices()
+      let audioOutputDevices = await VideoExpress.getAudioOutputDevices()
       audioOutputDevices = audioOutputDevices.map((audiooutput) =>
-        audiooutput.deviceId === 'default'
-          ? { ...audiooutput, label: 'System Default' }
+        audiooutput.deviceId === "default"
+          ? { ...audiooutput, label: "System Default" }
           : audiooutput
-      );
+      )
       const audioInputDevices = devices.filter(
-        (d) => d.kind.toLowerCase() === 'audioinput'
-      );
+        (d) => d.kind.toLowerCase() === "audioinput"
+      )
       const videoInputDevices = devices.filter(
-        (d) => d.kind.toLowerCase() === 'videoinput'
-      );
+        (d) => d.kind.toLowerCase() === "videoinput"
+      )
       setDeviceInfo({
         audioInputDevices,
         videoInputDevices,
         audioOutputDevices,
-      });
+      })
     } catch (err) {
-      console.log('[loadDevices] - ', err);
+      console.log("[loadDevices] - ", err)
     }
-  }, []);
+  }, [])
 
   useEffect(() => {
-    navigator.mediaDevices.addEventListener('devicechange', getDevices);
-    getDevices();
+    navigator.mediaDevices.addEventListener("devicechange", getDevices)
+    getDevices()
 
     return () => {
-      navigator.mediaDevices.removeEventListener('devicechange', getDevices);
-    };
-  }, [getDevices]);
+      navigator.mediaDevices.removeEventListener("devicechange", getDevices)
+    }
+  }, [getDevices])
 
-  return { deviceInfo, getDevices };
+  return { deviceInfo, getDevices }
 }
